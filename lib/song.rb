255c7f03b1efb85c76cfdf1b7f33903052623174
dwane richards <dwanerich@gmail.com>
2020-01-14 17:55:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,18 +36,11 @@ class Song
   def self.find_or_create_by_name(name)
       
       self.find_by_name(name) || self.create_by_name(name)
-      
-      # if self.find_by_name(name)
-      #   self.find_by_name(name)
-      # else
-      #   self.create_by_name(name)
-      # end
   end
   
   def self.alphabetical
       self.all.sort_by(&:name) 
       #{|name| song.name}
-      
   end
   
   def self.new_from_filename(filename)
